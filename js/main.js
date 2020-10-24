c94d9d14b8b8966454a5f8c5ba326d0c65a75cb1
sharkman11 <misha.akulov.11@mail.ru>
2020-10-24 21:34:51 +0300

    module6-task1

--- js/main.js
+++ js/main.js
@@ -5,4 +5,5 @@
   window.galleryModule.initGallery(posts);
   window.pictureModule.init();
   window.formModule.initForm();
+  window.load(window.galleryModule.initGallery, window.galleryModule.errorGalleryPictures);
 })();
